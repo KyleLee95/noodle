ca3f5a546aaabc9afeacfabe7921bcdfaf1a5fbf
kyle <kyle@kylelee.dev>
2025-01-15 14:54:24 -0500

    add 3d viewer to main app view

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,62 @@ import {
   Controls,
   Background,
   MiniMap,
+  ReactFlowProvider,
   useNodesState,
   useEdgesState,
   addEdge,
-  applyEdgeChanges,
-  applyNodeChanges,
+  type OnConnect,
+  type Edge,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import { useState, useCallback } from "react";
+import { useState, useRef, useCallback } from "react";
 import { type MyNode } from "@/components/flow/initial-elements";
 import TextNode from "@/components/flow/TextNode";
 import ResultNode from "@/components/flow/ResultNode";
 import UppercaseNode from "@/components/flow/UppercaseNode";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+const Viewer = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+};
 
 const nodeTypes = {
   text: TextNode,
@@ -87,33 +131,36 @@ function Flow() {
 
   return (
     <div className="h-full w-full">
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        nodeTypes={nodeTypes}
-        colorMode="system"
-        fitView
-      >
-        <Background />
-        <MiniMap />
-        <Controls />
-      </ReactFlow>
+      <ReactFlowProvider>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          nodeTypes={nodeTypes}
+          colorMode="system"
+          fitView
+        >
+          <Background />
+          <MiniMap />
+          <Controls />
+        </ReactFlow>
+      </ReactFlowProvider>
     </div>
   );
 }
 
 const App = () => {
   return (
-    <ResizablePanelGroup direction="vertical">
+    <ResizablePanelGroup direction="horizontal">
       <ResizablePanel>
         <Flow />
       </ResizablePanel>
-
       <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={25}>Two</ResizablePanel>
+      <ResizablePanel defaultSize={25}>
+        <Viewer />
+      </ResizablePanel>
     </ResizablePanelGroup>
   );
 };
